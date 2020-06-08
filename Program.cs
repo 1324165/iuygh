37f7d02512dc395657aecbcced41bfd7c6481d67
1324165 <66644775+1324165@users.noreply.github.com>
2020-06-09 00:33:20 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,44 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1._1._10._2D_ARRAY
+namespace _1._2._4.VALIDATOR
 {
     class Program
     {
-        static void printArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); ++i)
-            {
-                for (int j = 0; j < array.GetLength(1); ++j)
-                {
-                    Console.Write("{0}, ", array[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-
         static void Main(string[] args)
         {
-            int[,] array = new int[5, 5];
-            Random random = new Random();
-            for (int i = 0; i < array.GetLength(0); ++i)
-                for (int j = 0; j < array.GetLength(1); ++j)
+            Console.Write("IN: ");
+            string str1 = " " + Console.ReadLine();
+            char[] separators = new char[str1.Length / 3];
+            int j = 0;
+            for (int i = 0; i < str1.Length; ++i)//заполнение массива разделителей из введеной строки, чтоб не потерять знаки при выводе
+            {
+                if (str1[i] == '.' || str1[i] == '!' || str1[i] == '?')
                 {
-                    array[i, j] = random.Next(0, 100);
+                    separators[j] = str1[i];
+                    ++j;
                 }
-            Console.WriteLine("initial array:");
-            printArray(array);
-            int sum = 0;
-            for (int i = 0; i < array.GetLength(0); ++i)
-                for (int j = 0; j < array.GetLength(1); ++j)
-                {
-                    if ((i + j) % 2 == 0)
-                    {
-                        sum += array[i, j];
-                    }
-                }
-            Console.WriteLine(sum);
+            }
+            string[] sentenceArray = str1.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string strOut = "";
+
+            for (int i = 0; i < sentenceArray.Length; ++i)
+            {
+                string str2 = sentenceArray[i];
+                strOut = str2.ToUpper()[1].ToString();
+                str2 = str2.Substring(2);
+                strOut += str2;
+                Console.Write("{0}{1} ", strOut, separators[i]);
+            }
+
             Console.ReadLine();
+
         }
     }
 }
